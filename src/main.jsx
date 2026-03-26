36c6d9a037d3830bccd2042cb06490386721a927
Nicolas Card <nico@viscvle.de>
2026-03-26 10:32:48 +0100

    feat: add @vercel/speed-insights and inject into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 inject()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
+        <SpeedInsights />
     </React.StrictMode>,
 )
